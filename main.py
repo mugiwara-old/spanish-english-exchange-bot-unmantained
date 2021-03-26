67b1c6d5653b375948374e262a91fa4139fcd0c4
mugiwarafx <sangalesbenes@uoc.edu>
2021-03-27 00:00:10 +0100

    freecodecamp boilerplate

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}!'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hi'):
+        await message.channel.send('Hello!')
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
